73fbb39b3d2ae9fbbc9608df70abeb8184735540
Yuya Oguchi <yuyaoguchi@Yuyas-MacBook-Pro.local>
2018-03-03 15:47:30 -0800

    getplayer function and catching non 200 errors

--- datamine.py
+++ datamine.py
@@ -6,15 +6,28 @@ def getMatches(AccountId, region, APIKEY):
     global Players
     global Matches
     APIstring = 'https://'+region+'.api.riotgames.com/lol/match/v3/matchlists/by-account/'+AccountId+'?api_key=' + APIKEY
-    print APIstring
-    data = json.load(urllib2.urlopen(APIstring))
-    matchlist = []
-    for matches in data['matches']:
-        Matches.append(matches['gameId'])
+    try:
+        data = json.load(urllib2.urlopen(APIstring))
+        for matches in data['matches']:
+            Matches.append(matches['gameId'])
+    except urllib2.HTTPError, e:
+        print "Data returned error"
+        print e.code
 
 
 def getPlayers(MatchID, APIKEY):
+    global Players
+    global Matches
     APIstring = 'https://na1.api.riotgames.com/lol/match/v3/matches/'+MatchID+'?api_key=' + APIKEY
+    try:
+        data = json.load(urllib2.urlopen(APIstring))
+        for players in data['participantIdentities']:
+            Players.append(players['player']['currentAccountId'])
+    except urllib2.HTTPError, e:
+        print "Data returned error"
+        print e.code
+
+
 
 Key='RGAPI-3ae5a333-a70e-4d2e-9cad-0d40910b3821'
 Players = []
@@ -25,6 +38,12 @@ getMatches('244721782','na1', Key)
 print "total matches",
 print len(Matches)
 print Matches
+#
+# Matches.append('2730022598')
+# getPlayers('2730022598',Key)
+# print "Players in a match",
+# print len(Players)
+# print Players
 
 
 
